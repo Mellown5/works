a3ead47f4d339e0ef2738653042a01adbd5fdf60
Dmitry <nektovd@mail.ru>
2020-10-26 18:54:45 +0300

    1

---
+++ JS/script.js
@@ -0,0 +1,51 @@
+"use strict";
+//const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?","");
+
+//const personalMovieDB = {
+    //count: numberOfFilms,
+   // movies: {},
+    //actors: {},
+    //genres: [],
+    //privat: false
+//};
+//const a = prompt("Один из последних просмотренных фильмов", ""),
+     // b = prompt("На сколько оцените его?",""),
+      //c = prompt("Один из последних просмотренных фильмов", ""),
+      //d = prompt("На сколько оцените его?","");
+//personalMovieDB.movies[a] = b;
+//console.log(personalMovieDB);
+
+
+if (4 == 4) {
+    console.log('ok');
+}   else {
+    console.log('error');
+}
+
+// const num = 60;
+// if (num  <  40) {
+//     console.log('Мало');
+// }   else if (num >  100) {
+//     console.log("Много");
+// }   else {
+//     console.log('ok');
+// }
+
+// (num == 50) ? console.log('ok') :  console.log('error');
+const num = 50;
+
+// работает со строками и со стрингом
+switch (num) {
+    case 49:
+        console.log('Fail');
+        break;
+    case 100:
+        console.log('Fail');
+        break;
+    case 50:
+        console.log('good');
+        break;
+    default:
+        console.log('не в этот раз')
+        break;
+}
